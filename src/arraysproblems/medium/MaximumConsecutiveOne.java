130cecf47e334c6281873cff28f11e363ca32e34
unknown <nethaji043@gmail.com>
2025-07-19 08:43:43 +0530

    New Problem added

--- src/arraysproblems/medium/MaximumConsecutiveOne.java
+++ src/arraysproblems/medium/MaximumConsecutiveOne.java
@@ -4,21 +4,17 @@ public class MaximumConsecutiveOne {
 
     public static void main(String[] args) {
 
-
         int [] arr = new int[]{1,1,1,1,0,1,1,1,1,1,0,1,1,1};
         int count =0;
         int max =0;
         for(int i =0;i<= arr.length-1;i++){
-
             if(arr[i]==1){
                 count++;
             }else {
                 max  = Math.max(max,count);
                 count=0;
-
             }
         }
         System.out.println(max);
-
     }
 }
